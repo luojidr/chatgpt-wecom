e3c08b3de62dec3c39ebff3a7823ad8673a6b954
dingxutao <dingxutao@huacemedia.com>
2024-06-11 14:38:55 +0800

    context to start app

--- runserver.py
+++ runserver.py
@@ -8,4 +8,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # flask --app runserver db [init, migrate, upgrade, ...]
-    app.run(host='127.0.0.1', port=9999, debug=True)  # 非命令启动
+    with app.app_context():
+        app.run(host='127.0.0.1', port=9999, debug=True)  # 非命令启动
