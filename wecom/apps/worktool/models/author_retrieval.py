10053d0b2c50bfa7cd72554a7fee0fed28ea1bc2
dingxutao <dingxutao@huacemedia.com>
2024-07-10 09:50:05 +0800

    新增头部作者检索结果表

--- wecom/apps/worktool/models/author_retrieval.py
+++ wecom/apps/worktool/models/author_retrieval.py
@@ -7,20 +7,30 @@ from wecom.core.database import db, Column, BaseModel
 class AuthorRetrieval(BaseModel):
     __tablename__ = 'wecom_author_retrieval'
 
+    author = Column(db.String(100), nullable=False, server_default='')                      # 作者
+    platform = Column(db.String(100), nullable=False, server_default='')                    # 平台
+    brief = Column(db.String(300), nullable=False, server_default='')                       # 作者简介
+    is_adapt = Column(db.Boolean, nullable=False, default=False, server_default='0')        # 是否有影视改编作品
+    is_enabled = Column(db.Boolean, nullable=False, default=False, server_default='0')      # 是否可用
+
     # uniq: 弃用工作流运行结果rid，采用本地调用，随机生成 session_id
     uniq = Column(db.String(100), nullable=False, unique=True, server_default='')
     bing_results = Column(db.Text, nullable=False, server_default='')                        # BingSearch结果
-    llm_content = Column(db.String(5000), nullable=False, server_default='')                     # 大模型调用的结果
+    llm_content = Column(db.String(5000), nullable=False, server_default='')                 # 大模型调用的结果
+    is_delete = Column(db.Boolean, nullable=False, default=False, server_default='0')        # 是否删除
 
     @classmethod
-    def create(cls, uniq: str, bing_results: List[Dict[str, Any]], llm_content: str):
-        instance = cls(
-            uniq=uniq,
-            bing_results=json.loads(bing_results),
-            llm_content=llm_content
-        )
+    def create(cls, **kwargs):
+        fields = cls.fields()
+        values = {key: val for key, val in kwargs.items() if key in fields}
+
+        instance = cls(**values)
 
         db.session.add(instance)
         db.session.commit()
 
         return instance
+
+    @classmethod
+    def get_author_retrieval(cls, author: str, platform: str):
+        return cls.query.filter_by(author=author, platform=platform, is_delete=False).first()
